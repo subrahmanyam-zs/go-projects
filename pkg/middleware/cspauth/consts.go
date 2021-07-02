974daeee67a32d66da0f9d2c6125c57644222ecb
Utsav Vaghani <utsav.vaghani@zopsmart.com>
2021-07-02 12:35:51 +0530

    ID #57 | change the keylen in createkey and change sha256

--- pkg/middleware/cspauth/consts.go
+++ pkg/middleware/cspauth/consts.go
@@ -1,10 +1,9 @@
 package cspauth
 
 const (
-	encryptionBlockSizeBytes = 16
-	cspEncryptionIterations  = 1000
-	lenRandomChars           = 6
-	minAppKeyLen             = 12
-	appKeyHeader             = "ak"
-	authContextHeader        = "ac"
+	cspEncryptionIterations = 1000
+	lenRandomChars          = 6
+	minAppKeyLen            = 12
+	appKeyHeader            = "ak"
+	authContextHeader       = "ac"
 )
