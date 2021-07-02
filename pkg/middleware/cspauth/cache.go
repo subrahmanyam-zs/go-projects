071c0838315bbb7dc054477f053b68c0ae20ab9c
Aakanksha Jaiswal <aakanksha.jaiswal@zopsmart.com>
2021-07-02 11:25:02 +0530

    ID #57 | Add cache.go

---
+++ pkg/middleware/cspauth/cache.go
@@ -0,0 +1,45 @@
+package cspauth
+
+import "sync"
+
+type cache struct {
+	keys map[string]EncryptionKey
+	mu   sync.RWMutex
+}
+
+type EncryptionKey struct {
+	encryptionKey []byte // encryptionKey to be used for aes encryption/decryption
+	iv            []byte // initial vector(iv) to be used for aes encryption/decryption
+}
+
+func (c *cache) get(appKey string) EncryptionKey {
+	c.mu.Lock()
+
+	keys := c.keys[appKey]
+
+	c.mu.Unlock()
+
+	return keys
+}
+
+func (c *cache) set(appKey, sharedKey string) {
+	c.mu.Lock()
+
+	_, ok := c.keys[appKey]
+
+	c.mu.Unlock()
+
+	if !ok {
+		encryptionKey := createKey([]byte(appKey), []byte(appKey[:12]))
+		iv := createKey([]byte(sharedKey), []byte(appKey[:12]))
+
+		c.mu.Lock()
+
+		c.keys[appKey] = EncryptionKey{
+			encryptionKey: encryptionKey,
+			iv:            iv,
+		}
+
+		c.mu.Unlock()
+	}
+}
