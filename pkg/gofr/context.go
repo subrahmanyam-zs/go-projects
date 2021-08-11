af4e49f70701feb09c1707dc3c2598c43dd6f6ed
vaishnav-zopsmart <vaishnav.v@zopsmart.com>
2021-08-11 12:11:16 +0530

    refactor method Trace in context

--- pkg/gofr/context.go
+++ pkg/gofr/context.go
@@ -10,7 +10,6 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 
-	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/trace"
 
 	"developer.zopsmart.com/go/gofr/pkg/datastore/pubsub"
@@ -56,7 +55,7 @@ func (c *Context) reset(w responder.Responder, r request.Request) {
 
 // Trace returns an open telemetry span. We have to always close the span after corresponding work is done.
 func (c *Context) Trace(name string) trace.Span {
-	tr := otel.GetTracerProvider().Tracer("gofr-context")
+	tr := trace.SpanFromContext(c).TracerProvider().Tracer("gofr-context")
 	_, span := tr.Start(c.Context, name)
 	return span
 }
