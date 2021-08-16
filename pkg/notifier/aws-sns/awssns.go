c162af170b30675bc4126fe5239cf097d496fa71
Utsavkumar Vaghani <utsav_vaghani@mcafee.com>
2021-08-16 15:05:03 +0530

    Change the name of the metrics of notifier (#17)

--- pkg/notifier/aws-sns/awssns.go
+++ pkg/notifier/aws-sns/awssns.go
@@ -34,17 +34,17 @@ type Config struct {
 //nolint // The declared global variable can be accessed across multiple functions
 var (
 	notifierReceiveCount = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: "thanos_notifier_receive_count",
+		Name: "zs_notifier_receive_count",
 		Help: "Total number of subscribe operation",
 	}, []string{"topic"})
 
 	notifierSuccessCount = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: "thanos_notifier_success_count",
+		Name: "zs_notifier_success_count",
 		Help: "Total number of successful subscribe operation",
 	}, []string{"topic"})
 
 	notifierFailureCount = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: "thanos_notifier_failure_count",
+		Name: "zs_notifier_failure_count",
 		Help: "Total number of failed subscribe operation",
 	}, []string{"topic"})
 )
