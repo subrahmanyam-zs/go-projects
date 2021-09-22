34192bb036c683d91da7f0b67e405249b7b06114
vvaishna <v_vaishnav@mcafee.com>
2021-09-22 16:16:59 +0530

    ERROR logs for 5xx Response (#92)
    * add log with error level for statuscode 5xx
    
    * add test cases

--- pkg/middleware/logging.go
+++ pkg/middleware/logging.go
@@ -52,6 +52,7 @@ type logger interface {
 	Debug(args ...interface{})
 	AddData(key string, value interface{})
 	Errorf(format string, a ...interface{})
+	Error(args ...interface{})
 }
 
 type contextKey string
@@ -96,11 +97,18 @@ func Logging(logger logger, omitHeaders string) func(inner http.Handler) http.Ha
 
 					// .well-known, swagger and metrics endpoints are logged in debug mode, so that it can be excluded
 					// from logs, as usually logs with level INFO or higher than INFO are logged
+
 					if ExemptPath(r) {
 						logger.Debug(&l)
 					} else {
 						logger.Log(&l)
 					}
+
+					if res.status >= http.StatusInternalServerError && res.status <= http.StatusNetworkAuthenticationRequired {
+						l.Type = "ERROR"
+						logger.Error(&l)
+					}
+
 				}
 			}(srw, r)
 
