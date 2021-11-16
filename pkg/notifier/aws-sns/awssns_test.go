2700549a67f8e80603fadcd4b3d5e039990f8d9f
chaithraav9998 <chaithra.av@zopsmart.com>
2021-11-16 21:18:08 +0530

    fixing TestSNS_SubscribeWithResponse

--- pkg/notifier/aws-sns/awssns_test.go
+++ pkg/notifier/aws-sns/awssns_test.go
@@ -104,7 +104,7 @@ func TestSNS_SubscribeWithResponse(t *testing.T) {
 	}{
 		{
 			desc:   "Success Case",
-			expOut: &notifier.Message{Value: fmt.Sprintf(`{"SubscriptionArn":"%q"}`, svc.cfg.TopicArn)},
+			expOut: &notifier.Message{Value: fmt.Sprintf(`{"SubscriptionArn":"%s"}`, svc.cfg.TopicArn)},
 		},
 		{
 			desc:    "Failure Case",
