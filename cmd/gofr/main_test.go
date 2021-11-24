0e5df53b9125294522468ff8b5c9d33d73300564
vaishnav-zopsmart <vaishnav.v@zopsmart.com>
2021-11-24 17:48:59 +0530

    replace os.Mkdir to t.tempDir

--- cmd/gofr/main_test.go
+++ cmd/gofr/main_test.go
@@ -48,15 +48,11 @@ func Test_Migrate(t *testing.T) {
 	assert.CMDOutputContains(t, main, "gofr migrate -method=UP -database=gorm", "migrations do not exist")
 	assert.CMDOutputContains(t, main, "gofr migrate -method=UP -database=", "invalid flag: database")
 
-	currDir, _ := os.Getwd()
+	currDir := t.TempDir()
 	_ = os.Chdir(currDir)
-	_ = os.Mkdir("migrateCreateTest", migration.RWXMode)
-	_ = os.Chdir("migrateCreateTest")
 
-	defer func() {
-		_ = os.Chdir(currDir)
-		_ = os.RemoveAll("migrateCreateTest")
-	}()
+	path, _ := os.MkdirTemp(currDir, "migrateCreateTest")
+	_ = os.Chdir(path)
 
 	assert.CMDOutputContains(t, main, "gofr migrate create", "provide a name for migration")
 	assert.CMDOutputContains(t, main, "gofr migrate create -name=testMigration", "Migration created")
