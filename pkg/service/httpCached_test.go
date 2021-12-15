0c8a68caedbe61c199d2ba485bbc427bcfa5815c
asaxena5 <akash_saxena@mcafee.com>
2021-12-15 12:53:25 +0530

    #178 removed header propagation (#179)
    * #178 remove zopsmart-tenant and x-zopsmart-channel
    
    * added b3traceId
    
    * fix linters
    Co-authored-by: vaishnav <v_vaishnav@mcafee.com>

--- pkg/service/httpCached_test.go
+++ pkg/service/httpCached_test.go
@@ -196,15 +196,14 @@ func TestGetHeaders(t *testing.T) {
 }
 
 func Test_GetHeaders_All(t *testing.T) {
-	expectedHeaders := map[string]string{"X-Correlation-ID": "123", "True-Client-IP": "127.0.0.1",
-		"X-Zopsmart-Channel": "api", "X-Authenticated-UserId": "990", "X-Zopsmart-Tenant": "zopsmart"}
+	expectedHeaders := map[string]string{"X-Correlation-Id": "123", "True-Client-IP": "127.0.0.1",
+		"X-Authenticated-UserId": "990", "X-B3-TraceID": "123"}
 
 	ctx := context.TODO()
 	ctx = context.WithValue(ctx, middleware.CorrelationIDKey, "123")
 	ctx = context.WithValue(ctx, middleware.ClientIPKey, "127.0.0.1")
-	ctx = context.WithValue(ctx, middleware.ZopsmartChannelKey, "api")
 	ctx = context.WithValue(ctx, middleware.AuthenticatedUserIDKey, "990")
-	ctx = context.WithValue(ctx, middleware.ZopsmartTenantKey, "zopsmart")
+	ctx = context.WithValue(ctx, middleware.B3TraceIDKey, "123")
 
 	headers := cachedHTTPService{httpService: &httpService{}}.getHeaders(ctx, nil)
 
