f8396e6d064dd697171b4585868f6e19f5e8f3bb
vaishnav <v_vaishnav@mcafee.com>
2021-12-17 17:19:51 +0530

    handle 404 and 405 for healthCheck

--- pkg/gofr/health-check-server.go
+++ pkg/gofr/health-check-server.go
@@ -2,19 +2,29 @@ package gofr
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/gorilla/mux"
+
+	"developer.zopsmart.com/go/gofr/pkg/log"
+	"developer.zopsmart.com/go/gofr/pkg/middleware"
 )
 
 func healthCheckHandlerServer(ctx *Context, port int) *http.Server {
 	r := mux.NewRouter()
-	healthResp, err := HealthHandler(ctx)
+
+	// handles 404
+	r.NotFoundHandler = r.NewRoute().HandlerFunc(http.NotFound).GetHandler()
+
+	r.Use(validateRoutes(ctx.Logger))
 
 	r.HandleFunc("/.well-known/health-check", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 
+		healthResp, err := HealthHandler(ctx)
 		if err != nil {
 			ctx.Logger.Error(err)
 
@@ -45,4 +55,32 @@ func healthCheckHandlerServer(ctx *Context, port int) *http.Server {
 	})
 
 	return &http.Server{Addr: ":" + strconv.Itoa(port), Handler: r}
+}
+
+func validateRoutes(log log.Logger) func(http.Handler) http.Handler {
+	return func(inner http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			path := fmt.Sprintf("%s", req.URL.Path)
+
+			if !strings.Contains("/.well-known/health-check", path) {
+				err := middleware.FetchErrResponseWithCode(http.StatusNotFound,
+					fmt.Sprintf("Route %v not found", req.URL), "Invalid Route")
+
+				middleware.ErrorResponse(w, req, log, *err)
+
+				return
+			}
+
+			if req.Method != http.MethodGet {
+				err := middleware.FetchErrResponseWithCode(http.StatusMethodNotAllowed,
+					fmt.Sprintf("%v method not allowed for Route %v", req.Method, req.URL), "Invalid Method")
+
+				middleware.ErrorResponse(w, req, log, *err)
+
+				return
+			}
+
+			inner.ServeHTTP(w, req)
+		})
+	}
 }
